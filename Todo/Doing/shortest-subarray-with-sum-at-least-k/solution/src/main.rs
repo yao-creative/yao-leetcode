0dcfe75e0a8591c41c35bdca2f58b281a8fcb402
lema-founders <yytanwork@gmail.com>
2026-07-17 18:57:14 +0800

    added algebraic algorithms

---
+++ Todo/Doing/shortest-subarray-with-sum-at-least-k/solution/src/main.rs
@@ -0,0 +1,18 @@
+
+/// Solution trait.
+pub trait Solution {
+    /// Input is a sequence of integers.
+    fn shortest_subarray_at_least_k(&self, nums: Vec<i32>, k: i32) -> i32;
+}
+
+/// Solution impl.
+pub struct Solution1;
+
+impl Solution for Solution1 {
+    fn shortest_subarray_at_least_k(&self, nums: Vec<i32>, k: i32) -> i32 {
+        todo!()
+    }
+}
+
+
+
